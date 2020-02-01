802e90fe6fe63a87ea70b47f548bf521324b1d2d
elaine <elainedb.dev@gmail.com>
2020-02-01 16:02:26 +0100

    dependencies version bump

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,19 +3,84 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<apple_sign_in/AppleSignInPlugin.h>)
 #import <apple_sign_in/AppleSignInPlugin.h>
+#else
+@import apple_sign_in;
+#endif
+
+#if __has_include(<cloud_firestore/CloudFirestorePlugin.h>)
 #import <cloud_firestore/CloudFirestorePlugin.h>
+#else
+@import cloud_firestore;
+#endif
+
+#if __has_include(<device_info/DeviceInfoPlugin.h>)
 #import <device_info/DeviceInfoPlugin.h>
+#else
+@import device_info;
+#endif
+
+#if __has_include(<firebase_analytics/FirebaseAnalyticsPlugin.h>)
 #import <firebase_analytics/FirebaseAnalyticsPlugin.h>
+#else
+@import firebase_analytics;
+#endif
+
+#if __has_include(<firebase_auth/FirebaseAuthPlugin.h>)
 #import <firebase_auth/FirebaseAuthPlugin.h>
+#else
+@import firebase_auth;
+#endif
+
+#if __has_include(<firebase_core/FirebaseCorePlugin.h>)
 #import <firebase_core/FirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<firebase_crashlytics/FirebaseCrashlyticsPlugin.h>)
 #import <firebase_crashlytics/FirebaseCrashlyticsPlugin.h>
+#else
+@import firebase_crashlytics;
+#endif
+
+#if __has_include(<firebase_performance/FirebasePerformancePlugin.h>)
 #import <firebase_performance/FirebasePerformancePlugin.h>
+#else
+@import firebase_performance;
+#endif
+
+#if __has_include(<google_sign_in/GoogleSignInPlugin.h>)
 #import <google_sign_in/GoogleSignInPlugin.h>
+#else
+@import google_sign_in;
+#endif
+
+#if __has_include(<package_info/PackageInfoPlugin.h>)
 #import <package_info/PackageInfoPlugin.h>
+#else
+@import package_info;
+#endif
+
+#if __has_include(<quick_actions/QuickActionsPlugin.h>)
 #import <quick_actions/QuickActionsPlugin.h>
+#else
+@import quick_actions;
+#endif
+
+#if __has_include(<share/SharePlugin.h>)
 #import <share/SharePlugin.h>
+#else
+@import share;
+#endif
+
+#if __has_include(<shared_preferences/SharedPreferencesPlugin.h>)
 #import <shared_preferences/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
