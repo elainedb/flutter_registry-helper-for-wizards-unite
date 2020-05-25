cddb2ee285e34de380136075d56a42f4a21afc95
elaine <elainedb.dev@gmail.com>
2020-05-25 19:51:06 -0300

    WIP, finally got an idea. register boolean printed in sliver.dart in a store, get it on my_registry and change scrollphysics from never to scrollphysics

--- lib/pages/my_registry.dart
+++ lib/pages/my_registry.dart
@@ -34,6 +34,7 @@ class MyRegistryPageState extends State<MyRegistryPage> {
   GlobalKey globalKey3 = GlobalKey();
   GlobalKey globalKey4 = GlobalKey();
   bool _tutorialShown;
+  ScrollPhysics _scrollPhysics;
 
   final authentication = GetIt.instance<Authentication>();
   final registryStore = GetIt.instance<RegistryStore>();
@@ -50,6 +51,9 @@ class MyRegistryPageState extends State<MyRegistryPage> {
       viewportBoundaryGetter: () => Rect.fromLTRB(0, 0, 0, MediaQuery.of(context).padding.bottom),
       axis: Axis.vertical,
     );
+
+//    _scrollPhysics = NeverScrollableScrollPhysics();
+    _scrollPhysics = ScrollPhysics();
   }
 
   @override
@@ -69,32 +73,23 @@ class MyRegistryPageState extends State<MyRegistryPage> {
     return Row(
       children: <Widget>[
         Expanded(
-          child: Builder(builder: (BuildContext context) {
-            return CustomScrollView(
-              slivers: <Widget>[
-                SliverOverlapInjector(
-                  // This is the flip side of the SliverOverlapAbsorber above.
-                  handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-                ),
-                SliverList(
-                  delegate: SliverChildListDelegate(
-                    <Widget>[
-                      chapterCard("cmc", AppColors.cmcDark, AppColors.cmcLight, 0),
-                      chapterCard("da", AppColors.daDark, AppColors.daLight, 1),
-                      chapterCard("hs", AppColors.hsDark, AppColors.hsLight, 2),
-                      chapterCard("loh", AppColors.lohDark, AppColors.lohLight, 3),
-                      chapterCard("mom", AppColors.momDark, AppColors.momLight, 4),
-                      chapterCard("m", AppColors.mDark, AppColors.mLight, 5),
-                      chapterCard("mgs", AppColors.mgsDark, AppColors.mgsLight, 6),
-                      chapterCard("mar", AppColors.maDark, AppColors.maLight, 7),
-                      chapterCard("www", AppColors.wwwDark, AppColors.wwwLight, 8),
-                      chapterCard("o", AppColors.oDark, AppColors.oLight, 9),
-                    ],
-                  ),
-                )
-              ],
-            );
-          }),
+          child: ListView(
+            physics: _scrollPhysics,
+            scrollDirection: Axis.vertical,
+            controller: controller,
+            children: <Widget>[
+              chapterCard("cmc", AppColors.cmcDark, AppColors.cmcLight, 0),
+              chapterCard("da", AppColors.daDark, AppColors.daLight, 1),
+              chapterCard("hs", AppColors.hsDark, AppColors.hsLight, 2),
+              chapterCard("loh", AppColors.lohDark, AppColors.lohLight, 3),
+              chapterCard("mom", AppColors.momDark, AppColors.momLight, 4),
+              chapterCard("m", AppColors.mDark, AppColors.mLight, 5),
+              chapterCard("mgs", AppColors.mgsDark, AppColors.mgsLight, 6),
+              chapterCard("mar", AppColors.maDark, AppColors.maLight, 7),
+              chapterCard("www", AppColors.wwwDark, AppColors.wwwLight, 8),
+              chapterCard("o", AppColors.oDark, AppColors.oLight, 9),
+            ],
+          ),
         ),
         Padding(
           padding: AppStyles.registryIndexInsets,
--- lib/sliver.dart
+++ lib/sliver.dart
@@ -33,34 +33,39 @@ class SliverWidgetState extends State<SliverWidget> with SingleTickerProviderSta
     return DefaultTabController(
       initialIndex: 1,
       length: 2,
-      child: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return [
-            SliverOverlapAbsorber(
-              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-              sliver: SliverAppBar(
-                expandedHeight: 150,
-                floating: false,
-                pinned: false,
-                bottom: TabBar(
-                  controller: _controller,
-                  labelColor: Colors.amber,
-                  indicatorColor: Colors.amber,
-                  tabs: [
-                    Tab(text: "missing_foundables_title".i18n()),
-                    Tab(text: "insights".i18n()),
-                  ],
+      child: NotificationListener<ScrollUpdateNotification>(
+        onNotification: (notification) {
+          print("2 ${notification.metrics.pixels >= (150 + MediaQuery.of(context).padding.top)}");
+        },
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              SliverOverlapAbsorber(
+                handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                sliver: SliverAppBar(
+                  expandedHeight: 150,
+                  floating: false,
+                  pinned: false,
+                  bottom: TabBar(
+                    controller: _controller,
+                    labelColor: Colors.amber,
+                    indicatorColor: Colors.amber,
+                    tabs: [
+                      Tab(text: "missing_foundables_title".i18n()),
+                      Tab(text: "insights".i18n()),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ];
-        },
-        body: TabBarView(
-          controller: _controller,
-          children: [
-            BottomBarNavWidget(),
-            BottomBarNavWidget(),
-          ].toList(),
+            ];
+          },
+          body: TabBarView(
+            controller: _controller,
+            children: [
+              BottomBarNavWidget(),
+              BottomBarNavWidget(),
+            ].toList(),
+          ),
         ),
       ),
     );
